343031d73a135e996f11b9ba82a5d833bf1feea2
Frank <s8frlore@stud.uni-saarland.de>
2022-04-14 18:50:02 +0200

    UI Updated

---
+++ NegotiationGame.v2/Client/GameLogic/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NegotiationGame.v2.Client.GameLogic
+{
+    public class ChatMessage
+    {
+        public ChatMessage(string message, DateTime time, string username)
+        {
+            Message = message;
+            Time = time;
+            Username = username;
+        }
+
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public string Username { get; set; }
+    }
+}
